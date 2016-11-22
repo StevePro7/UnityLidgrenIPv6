979c36b7e1773cda8795dba2217b0a5811fbbf49
StevePro7 <steveprozune@hotmail.com>
2016-11-22 20:35:20 +0000

    Remove extra files

--- IPv6/GameUnity/Assets/Command/IOSBuildBETA.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-/Applications/Unity/Unity.app/Contents/MacOS/Unity -quit -batchmode -nographics -executeMethod PerformBuild.CommandLineBuildOnCheckinIOS -CustomArgs:Environment=IPv4;Version=1;ShortBundleVersion=1;BundleID=com.steveproxna.gameunity;SvnRevision=${SVN_REVISION}
